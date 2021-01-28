d534785abde2828c01d8ffaa576404add78d8013
Changming Sun <chasun@microsoft.com>
2021-01-27 17:43:31 -0800

    update

--- tools/ci_build/github/windows/jar_packaging.ps1
+++ tools/ci_build/github/windows/jar_packaging.ps1
@@ -1,13 +1,16 @@
 $ErrorActionPreference = "Stop"
 Set-PSDebug -Trace 1
+Write-Output "Start"
 dir
+Write-Output "pushd onnxruntime-java-linux-x64"
 pushd onnxruntime-java-linux-x64
-jar uf  $Env:BUILD_BINARIESDIRECTORY\java-artifact\onnxruntime-java-win-x64\testing.jar libcustom_op_library.so
+Write-Output "Run 7z"
+7z a $Env:BUILD_BINARIESDIRECTORY\java-artifact\onnxruntime-java-win-x64\testing.jar libcustom_op_library.so
 Remove-Item -Path libcustom_op_library.so
-jar uf  $Env:BUILD_BINARIESDIRECTORY\java-artifact\onnxruntime-java-win-x64\onnxruntime-$Env:ONNXRUNTIMEVERSION.jar .
+7z a $Env:BUILD_BINARIESDIRECTORY\java-artifact\onnxruntime-java-win-x64\onnxruntime-$Env:ONNXRUNTIMEVERSION.jar .
 popd
 pushd onnxruntime-java-osx-x64
-jar uf  $Env:BUILD_BINARIESDIRECTORY\java-artifact\onnxruntime-java-win-x64\testing.jar libcustom_op_library.dylib
+7z a $Env:BUILD_BINARIESDIRECTORY\java-artifact\onnxruntime-java-win-x64\testing.jar libcustom_op_library.dylib
 Remove-Item -Path libcustom_op_library.dylib
-jar uf  $Env:BUILD_BINARIESDIRECTORY\java-artifact\onnxruntime-java-win-x64\onnxruntime-$Env:ONNXRUNTIMEVERSION.jar .
+7z a $Env:BUILD_BINARIESDIRECTORY\java-artifact\onnxruntime-java-win-x64\onnxruntime-$Env:ONNXRUNTIMEVERSION.jar .
 popd
